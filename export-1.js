5bba9dbe13567814da0c4ba2dfd1dee05755fa57
edi vidrascu <edividrascu@gmail.com>
2024-03-29 19:17:26 +0100

    ejercicio 3 export

---
+++ export-1.js
@@ -0,0 +1,3 @@
+const mensajeDeSlida = require('./import-1');
+
+mensajeDeSlida("ignore los 45 errores graves");
---
+++ import-1.js
@@ -0,0 +1,4 @@
+function mensajeDeSlida(params) {
+    console.log("el programa funciona corectamente " + params);
+}
+module.exports = mensajeDeSlida;
